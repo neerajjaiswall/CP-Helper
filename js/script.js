c3c91587fe64d5200a0da1c6970761b480ec56a1
sourcedc <maceleven68@gmail.com>
2021-03-11 12:46:00 +0530

    Added Forms with their unique IDs from tem.htm to index page, and set all their onclick functionalities using the initial template in code.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
-var forms = ["numberForm", "simpleArrayForm",  ]
+var forms = ["numberForm", "simpleArrayForm", "arrayOfPairsForm", "arrayMatrixForm", "simpleStringForm", "characterMatrixForm", "unweightedTreeForm", "weightedTreeForm", "unweightedGraphForm", "weightedGraphForm" ]
 
-document.getElementById("number").onclick = function() {
-  var element = document.getElementById("number");
-  element.classList.add("active");
-  var element = document.getElementById("numberForm");
-  element.classList.remove("d-none");
-}
+let tabs = []
+forms.forEach( (form)=>{ tabs.push(form.substring(0, form.indexOf("Form"))) } )
 
-document.getElementById("simpleArray").onclick = function() {
-  var element = document.getElementById("array");
-  element.classList.add("active");
-  var element = document.getElementById("simpleArrayForm");
-  element.classList.remove("d-none");
-  var element = document.getElementById("number");
-  element.classList.remove("active");
-  var element = document.getElementById("numberForm");
-  element.classList.add("d-none");
-}
+// document.getElementById("number").onclick = function() {
+//   var element = document.getElementById("number");
+//   element.classList.add("active");
+//   forms.forEach( (formElement)=>{ $("#"+formElement).classList.add('d-none') } )
+//   document.getElementById("numberForm").classList.remove("d-none");
+// }
 
+// document.getElementById("simpleArray").onclick = function() {
+//   var element = document.getElementById("array");
+//   element.classList.add("active");
+//   forms.forEach( (formElement)=>{ $("#"+formElement).classList.add('d-none') } )
+//   document.getElementById("simpleArrayForm").classList.remove("d-none");
+//   var element = document.getElementById("number");
+//   element.classList.remove("active");
+//   var element = document.getElementById("numberForm");
+//   element.classList.add("d-none");
+// }
+
+tabs.forEach(tab => {
+  document.getElementById(tab).onclick = () => {
+    tabs.forEach( item => { document.getElementById(item).classList.remove('active') } )
+    var element = document.getElementById(tab);
+    element.classList.add("active");
+    forms.forEach( (formElement)=>{ document.getElementById(formElement).classList.add('d-none') } )
+    document.getElementById(tab+"Form").classList.remove("d-none");  
+  }
+})
