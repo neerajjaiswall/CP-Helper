e495d31a2ed65136f4ff97e68f8757e321e1c06e
Neeraj Jaiswal <neerajjaiswal8952@gmail.com>
2021-03-11 10:52:31 +0530

    new forms added
    solved display problem.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
-document.getElementById("numlink").onclick = function() {
-  var element = document.getElementById("numlink");
+var forms = ["numberForm", "simpleArrayForm",  ]
+
+document.getElementById("number").onclick = function() {
+  var element = document.getElementById("number");
   element.classList.add("active");
-  document.getElementById("s_array").style.display = "none";
-  document.getElementById("just_number").style.display  = "inline";
+  var element = document.getElementById("numberForm");
+  element.classList.remove("d-none");
+}
+
+document.getElementById("simpleArray").onclick = function() {
+  var element = document.getElementById("array");
+  element.classList.add("active");
+  var element = document.getElementById("simpleArrayForm");
+  element.classList.remove("d-none");
+  var element = document.getElementById("number");
+  element.classList.remove("active");
+  var element = document.getElementById("numberForm");
+  element.classList.add("d-none");
 }
 
