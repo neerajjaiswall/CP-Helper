b9975264301dcc8a9107a90f9a458fdef6c420b8
Neeraj Jaiswal <neerajjaiswal8952@gmail.com>
2021-03-11 17:59:27 +0530

    all forms fitted.
    whole forntend completed for generator part.

--- js/script.js
+++ js/script.js
@@ -1,21 +1,83 @@
-var forms = ["numberForm", "simpleArrayForm", "arrayOfPairsForm", "matrixForm", "simpleStringForm", "characterMatrixForm", "unweightedTree", "weightedTreeForm", "unweightedGraphForm", "weightedGraphForm"]
+var forms = ["singleNumberForm", "simpleArrayForm", "arrayOfPairsForm", "matrixForm", "simpleStringForm", "characterMatrixForm", "unweightedTreeForm", "weightedTreeForm", "unweightedGraphForm", "weightedGraphForm"];
+var categories = ["number", "array", "string", "tree", "graph"];
+function showForm() {
+  var anchorlink = arguments[0];
+  // console.log(anchorlink);
+  var i;
+  for(i=0; i<forms.length; i++) {
+    var element = document.getElementById(forms[i]);
+    if(!element.classList.contains("d-none"))
+      element.classList.add("d-none");
+  }
 
-funtion
-document.getElementById("number").onclick = function () {
-  var element = document.getElementById("number");
-  element.classList.add("active");
-  var element = document.getElementById("numberForm");
-  element.classList.remove("d-none");
+  for(i=0; i<categories.length; i++) {
+    var element = document.getElementById(categories[i]);
+    if(element.classList.contains("active"))
+      element.classList.remove("active");
+  }
+  var element = document.getElementById(arguments[0]);
+  element = anchorlink;
+  var formId, categoryId;
+  if(element == "singleNumber") {
+    formId = "singleNumberForm";
+    categoryId = "number";
+  }
+  if(element == "simpleArray") {
+    formId = "simpleArrayForm";
+    categoryId = "array";
+  }
+  if(element == "arrayOfPairs") {
+    formId = "arrayOfPairsForm";
+    categoryId = "array";
+  }
+  if(element == "matrix") {
+    formId = "matrixForm";
+    categoryId = "array";
+  }
+  if(element == "simpleString") {
+    formId = "simpleStringForm";
+    categoryId = "string";
+  }
+  if(element == "characterMatrix") {
+    formId = "characterMatrixForm";
+    categoryId = "string";
+  }
+  if(element == "weightedTree") {
+    formId = "weightedTreeForm";
+    categoryId = "tree";
+  }
+  if(element == "unweightedTree") {
+    formId = "unweightedTreeForm";
+    categoryId = "tree";
+  }
+  if(element == "weightedGraph") {
+    formId = "weightedGraphForm";
+    categoryId = "graph";
+  }
+  if(element == "unweightedGraph") {
+    formId = "unweightedGraphForm";
+    categoryId = "graph";
+  }
+
+  document.getElementById(formId).classList.remove("d-none");
+  document.getElementById(categoryId).classList.add("active");
 }
 
-document.getElementById("simpleArray").onclick = function () {
-  var element = document.getElementById("array");
-  element.classList.add("active");
-  var element = document.getElementById("simpleArrayForm");
-  element.classList.remove("d-none");
-  var element = document.getElementById("number");
-  element.classList.remove("active");
-  var element = document.getElementById("numberForm");
-  element.classList.add("d-none");
-}
+// document.getElementById("number").onclick = function () {
+//   var element = document.getElementById("number");
+//   element.classList.add("active");
+//   var element = document.getElementById("numberForm");
+//   element.classList.remove("d-none");
+// }
+
+// document.getElementById("simpleArray").onclick = function () {
+//   var element = document.getElementById("array");
+//   element.classList.add("active");
+//   var element = document.getElementById("simpleArrayForm");
+//   element.classList.remove("d-none");
+//   var element = document.getElementById("number");
+//   element.classList.remove("active");
+//   var element = document.getElementById("numberForm");
+//   element.classList.add("d-none");
+// }
 
