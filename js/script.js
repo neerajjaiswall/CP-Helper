2bd1a0baa40082ed587bc86563c0157ea5f25b78
neerajjaiswall <neerajjaiswal8952@gmail.com>
2021-06-20 00:09:02 +0530

    added matrix and char matrix part
    added matrix and char matrix part

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-var forms = ["singleNumberForm", "simpleArrayForm", "arrayOfPairsForm", "matrixForm", "simpleStringForm", "characterMatrixForm", "unweightedTreeForm", "weightedTreeForm", "unweightedGraphForm", "weightedGraphForm"];
+var forms = [
+  "singleNumberForm",
+  "simpleArrayForm",
+  "arrayOfPairsForm",
+  "matrixForm",
+  "simpleStringForm",
+  "characterMatrixForm",
+  "unweightedTreeForm",
+  "weightedTreeForm",
+  "unweightedGraphForm",
+  "weightedGraphForm",
+];
 var categories = ["number", "array", "string", "tree", "graph"];
 function showForm() {
   var anchorlink = arguments[0];
@@ -6,8 +17,7 @@ function showForm() {
   var i;
   for (i = 0; i < forms.length; i++) {
     var element = document.getElementById(forms[i]);
-    if (!element.classList.contains("d-none"))
-      element.classList.add("d-none");
+    if (!element.classList.contains("d-none")) element.classList.add("d-none");
   }
 
   for (i = 0; i < categories.length; i++) {
@@ -76,9 +86,6 @@ function showForm() {
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function getRndChar() {
-  
-}
 
 function generateInput() {
   document.getElementById("outputTextArea").value = "";
@@ -88,22 +95,23 @@ function generateInput() {
     var n_test_cases = document.getElementById("numberTestCases").value;
     var min_value = document.getElementById("numberMinValue").value;
     var max_value = document.getElementById("numberMaxValue").value;
-    var n_test_cases_flag = document.getElementById("numberTestCasesFlag").value;
+    var n_test_cases_flag = document.getElementById(
+      "numberTestCasesFlag"
+    ).value;
 
     // var currentState = getTextAreaValue("outputTextArea");
     if (n_test_cases_flag == "1") {
-      document.getElementById("outputTextArea").value += n_test_cases + '\n';
+      document.getElementById("outputTextArea").value += n_test_cases + "\n";
     }
 
     var it;
     for (it = 0; it < n_test_cases; it++) {
-      document.getElementById("outputTextArea").value += getRndInteger(Number(min_value), Number(max_value)) + '\n';
+      document.getElementById("outputTextArea").value +=
+        getRndInteger(Number(min_value), Number(max_value)) + "\n";
     }
     // console.log(min_value + " " + max_value);
     // console.log(max_value);
-  }
-
-  else if (heading == "Random Simple Array") {
+  } else if (heading == "Random Simple Array") {
     var tc = document.getElementById("simpleArrayNtc").value;
     var cur_size = document.getElementById("simpleArraySize").value;
     var min_val = document.getElementById("simpleArrayMinValue").value;
@@ -115,66 +123,155 @@ function generateInput() {
     console.log(tc, cur_size, min_val, max_val, tcFlag, nFlag, dictinct_value);
     // var currentState = getTextAreaValue("outputTextArea");
     if (tcFlag == "1") {
-      document.getElementById("outputTextArea").value += tc + '\n';
+      document.getElementById("outputTextArea").value += tc + "\n";
     }
 
     var it, it2;
     for (it = 0; it < tc; it++) {
-      if (nFlag == "1") document.getElementById("outputTextArea").value += cur_size + '\n';
+      if (nFlag == "1")
+        document.getElementById("outputTextArea").value += cur_size + "\n";
       for (it2 = 0; it2 < cur_size; it2++) {
-        document.getElementById("outputTextArea").value += getRndInteger(Number(min_val), Number(max_val)) + ' ';
+        document.getElementById("outputTextArea").value +=
+          getRndInteger(Number(min_val), Number(max_val)) + " ";
       }
-      document.getElementById("outputTextArea").value += '\n';
+      document.getElementById("outputTextArea").value += "\n";
     }
-  }
-
-
-  else if(heading=="Random Simple String")
-  {
+  } else if (heading == "Random Simple String") {
     console.log("neeraj");
-    var tc =document.getElementById("simpleStringNtc").value;
-    var tcFlag=document.getElementById("simpleStringNtcFlag").value;
-    var s_size=document.getElementById("simpleStringSize").value;
-    var nflag=document.getElementById("simpleStringNFlag").value;
-    var specific_char=document.getElementById("simpleStringSpecificChar").value;
-    var s_char = (specific_char).split(",");
+    var tc = document.getElementById("simpleStringNtc").value;
+    var tcFlag = document.getElementById("simpleStringNtcFlag").value;
+    var s_size = document.getElementById("simpleStringSize").value;
+    var nflag = document.getElementById("simpleStringNFlag").value;
+    var specific_char = document.getElementById(
+      "simpleStringSpecificChar"
+    ).value;
+
+    var s_char;
+    if(specific_char!="")
+      s_char = specific_char.split(",");
+    else s_char = [];
     // var distinct_cflag=document.getelementById("simpleStringDcharFlag").value;
 
     if (tcFlag == "1") {
-      document.getElementById("outputTextArea").value += tc + '\n';
+      document.getElementById("outputTextArea").value += tc + "\n";
     }
-    var it,it2;
+    var it, it2;
     console.log(s_char);
-    if(s_char.length) {
+    if (s_char.length) {
       var siz = s_char.length;
       console.log(siz);
       console.log(Math.random());
-      for(it=0;it<tc;it++) {
-        if(nflag=="1") document.getElementById("outputTextArea").value += s_size + '\n';
-        for(it2=0; it2<s_size; it2++)
-          document.getElementById("outputTextArea").value += s_char[getRndInteger(Number(0), Number(1000000))%(siz)];
-          document.getElementById("outputTextArea").value +='\n';
+      for (it = 0; it < tc; it++) {
+        if (nflag == "1")
+          document.getElementById("outputTextArea").value += s_size + "\n";
+        for (it2 = 0; it2 < s_size; it2++)
+          document.getElementById("outputTextArea").value +=
+            s_char[getRndInteger(Number(0), Number(1000000)) % siz];
+        document.getElementById("outputTextArea").value += "\n";
       }
-    }
-    else {
-      for(it=0;it<tc;it++) {
-        if(nflag=="1") document.getElementById("outputTextArea").value += s_size + '\n';
-        for(it2=0; it2<s_size; it2++)
-          document.getElementById("outputTextArea").value += String.fromCharCode(97 + getRndInteger(Number(0), Number(25)));
-          document.getElementById("outputTextArea").value +='\n';
-      }
-    }
     } 
+    else {
+      for (it = 0; it < tc; it++) {
+        if (nflag == "1")
+          document.getElementById("outputTextArea").value += s_size + "\n";
+        for (it2 = 0; it2 < s_size; it2++)
+          document.getElementById("outputTextArea").value +=
+            String.fromCharCode(97 + getRndInteger(Number(0), Number(25)));
+        document.getElementById("outputTextArea").value += "\n";
+      }
+    }
+  } else if (heading == "Random Matrix") {
+    var tc = document.getElementById("matrixNtc").value;
+    var Rows = document.getElementById("matrixRows").value;
+    var Cols = document.getElementById("matrixCols").value;
+    var min_val = document.getElementById("matrixMinValue").value;
+    var max_val = document.getElementById("matrixMaxValue").value;
+    var tcFlag = document.getElementById("matrixNtcFlag").value;
+    var RCFlag = document.getElementById("matrixRCFlag").value;
+    var dictinct_value = document.getElementById("simpleArrayDvFlag").value;
+
+    // console.log(tc, cur_size, min_val, max_val, tcFlag, nFlag, dictinct_value);
+    // var currentState = getTextAreaValue("outputTextArea");
+    if (tcFlag == "1") {
+      console.log("nero");
+      document.getElementById("outputTextArea").value += tc + "\n";
+    }
+
+    var it, it2;
+    for (it = 0; it < tc; it++) {
+      if (RCFlag == "1")
+        document.getElementById("outputTextArea").value +=
+          Rows + " " + Cols + "\n";
+      var i, j;
+      for (i = 0; i < Rows; i++) {
+        for (j = 0; j < Cols; j++)
+          document.getElementById("outputTextArea").value +=
+            getRndInteger(Number(min_val), Number(max_val)) + " ";
+        document.getElementById("outputTextArea").value += "\n";
+      }
+    }
+  } else if (heading == "Random Character Matrix") {
+    var tc = document.getElementById("charMatrixNtc").value;
+    var Rows = document.getElementById("charMatrixRows").value;
+    var Cols = document.getElementById("charMatrixCols").value;
+    var tcFlag = document.getElementById("charMatrixNtcFlag").value;
+    var RCFlag = document.getElementById("charMatrixRClag").value;
+    var specific_char = document.getElementById(
+      "charMatrixSpecificChars"
+    ).value;
+    var s_char;
+    if(specific_char!="")
+      s_char = specific_char.split(",");
+    else s_char = [];
+
+    // console.log(tc, cur_size, min_val, max_val, tcFlag, nFlag, dictinct_value);
+    // var currentState = getTextAreaValue("outputTextArea");
+    if (tcFlag == "1") {
+      console.log("nero");
+      document.getElementById("outputTextArea").value += tc + "\n";
+    }
+
+
+    if (s_char.length) {
+      var siz = s_char.length;
+
+      var it, it2;
+      for (it = 0; it < tc; it++) {
+        if (RCFlag == "1")
+          document.getElementById("outputTextArea").value +=
+            Rows + " " + Cols + "\n";
+        var i, j;
+        for (i = 0; i < Rows; i++) {
+          for (j = 0; j < Cols; j++)
+            document.getElementById("outputTextArea").value +=
+            s_char[getRndInteger(Number(0), Number(1000000)) % siz] +
+              " ";
+          document.getElementById("outputTextArea").value += "\n";
+        }
+        // document.getElementById("outputTextArea").value += '\n';
+      }
+    } 
+
+    else {
+      var it, it2;
+      for (it = 0; it < tc; it++) {
+        if (RCFlag == "1")
+          document.getElementById("outputTextArea").value +=
+            Rows + " " + Cols + "\n";
+        var i, j;
+        for (i = 0; i < Rows; i++) {
+          for (j = 0; j < Cols; j++)
+            document.getElementById("outputTextArea").value +=
+              String.fromCharCode(97 + getRndInteger(Number(0), Number(25))) +
+              " ";
+          document.getElementById("outputTextArea").value += "\n";
+        }
+        // document.getElementById("outputTextArea").value += '\n';
+      }
+    }
+
+  }
 }
-
-
-
-
-
-
-
-
-
 
 // document.getElementById("number").onclick = function () {
 //   var element = document.getElementById("number");
@@ -193,4 +290,3 @@ function generateInput() {
 //   var element = document.getElementById("numberForm");
 //   element.classList.add("d-none");
 // }
-
